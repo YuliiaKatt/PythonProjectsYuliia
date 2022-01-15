e7a7574f2caa2adce6481df20e02a3c5c0d26c47
YuliiaKatt <yuliia.katt@gmail.com>
2022-01-15 02:15:42 +0200

    modified triangle.py

--- triangle.py
+++ triangle.py
@@ -1,34 +1,37 @@
-# Программа рисует фигуру С (ромб)
-# Пользователь вводит с клавиатуры высоту фигуры
-height_picture = int(input("Введите высоту рисунка: "))
+# Программа рисует фигуру D (ромб c полосой посередине)
 
-# Ширина рисунка
-length_picture = height_picture
+# Пользователь вводит с клавиатуры высоту фигуры
+height_picture = int(input("Введите высоту рисунка (нечетное число): "))
+
+# Делаем рисунок симметричным. Если пользователь введет четное число, то рисунок увеличится на 1 символ
+if height_picture % 2 == 0:
+    height_picture += 1
 
 # Ромб будет состоять из треугольников, где высота треугольников
 height_triangle = int(height_picture / 2) + 1
+
+# Ширина треугольников
+length_triangle = height_picture
 
 # Символы, которыми будет нарисована картинка
 star = "* "
 space = "  "
 
-# Первый треугольник
-for i in reversed(range(height_picture)):
-    picture1 = space * i + star * (length_picture - i * 2) + space * i
+# Рисуем первый треугольник (заполненный "*")
+for i in reversed(range(height_triangle)):
+    picture1 = space * i + star * (length_triangle - i * 2) + space * i
     print(picture1)
 
-# Второй треугольник
-for i in range(height_picture):
-    i += 1
-    picture2 = space * i + star * (length_picture - i * 2) + space * i
-
-    # if i == 0:
-    #     continue
-    elif i != (height_picture - 1):
-        picture3 = space * i + star + space * ((length_picture - 2) - i * 2) + star + space * i
+# Рисуем второй треугольник
+for i in range(height_triangle):
+    picture2 = space * i + star * (length_triangle - i * 2) + space * i
+    #  Пропускаем первую итерацию, чтоб не выводилось основание второго треугольника
+    if i == 0:
+        continue
+    #  Делаем треугольник пустым, с полосой посередине
+    elif i != (height_triangle - 1):
+        half_line = int((length_triangle - 2 - i * 2) / 2)
+        picture3 = space * i + star + space * half_line + star + space * half_line + star + space * i
         print(picture3)
-    # elif i == (length_picture - height_picture):
-    #     print(star)
-
     else:
         print(picture2)
