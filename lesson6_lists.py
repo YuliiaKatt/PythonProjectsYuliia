7ae67e044d700c5196b211355ecabda81640e4be
YuliiaKatt <yuliia.katt@gmail.com>
2022-01-17 23:35:27 +0200

    modified lesson6_lists.py

--- lesson6_lists.py
+++ lesson6_lists.py
@@ -17,5 +17,4 @@ height_students.sort(reverse=True)
 print("Список учеников (вместе с Петей), отсортированных по росту:\n", height_students)
 
 # Определяем место в строю для Пети
-any([x in height_students])
 print("Петя должен встать в строй под номером: ", students - height_students[::-1].index(x) + 1)
